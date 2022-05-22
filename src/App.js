c0d2e4fa033ecab1ba0689fa5def89f974796c63
mirajul alam <alifmirajulalam@gmail.comm>
2022-05-22 10:18:00 +0600

    home page banner added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Blogs from './Pages/Blogs/Blogs';
 import Home from './Pages/Home/Home';
 import Navbar from './Pages/Shared/Navbar';
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, [])
   return (
     <div>
       <Navbar />
---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import bannerImages from '../../assets/bannerImages.jpg'
+const Banner = () => {
+    return (
+        <div className="container">
+            <div className="hero-content flex-col lg:flex-row-reverse px-20 py-20 bg-base-100">
+                <img data-aos="zoom-in" data-aos-delay="200" data-aos-duration="1000" className='w-full lg:w-5/12' src={bannerImages} alt="" />
+                <div >
+                    <h1 data-aos="fade-right" data-aos-duration="1000" data-aos-delay="200" className="text-5xl font-bold">Car manufacturers make our own parts</h1>
+                    <p data-aos="fade-right" data-aos-duration="900" data-aos-delay="400" className="py-6">Virtually all automotive manufacturers produce their own parts. However, over the years, manufacturers have been turning to first-tier suppliers for the production of just-in-time components. For this reason, many parts suppliers are now as global as the vehicle manufacturers themselves.</p>
+                    <button class="btn btn-primary" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="600">Get Started</button>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Banner;
